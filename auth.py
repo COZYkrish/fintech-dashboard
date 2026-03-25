0996b470f5edf47755b648e422eb75549979a942
COZYkrish <cozykrish2916@gmail.com>
2026-03-25 12:00:00 +0530

    updated chart recommendation

--- auth.py
+++ auth.py
@@ -8,10 +8,10 @@ app.secret_key = 'your_secret_key_here'
 
 USERS_FILE = 'users.json'
 
-# def load_users():
-#     if os.path.exists(USERS_FILE):
-#         with open(USERS_FILE, 'r') as f:
-#             try:
+def load_users():
+    if os.path.exists(USERS_FILE):
+        with open(USERS_FILE, 'r') as f:
+            try:
                 return json.load(f)
             except json.JSONDecodeError:
                 return {}
