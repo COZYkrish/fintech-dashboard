007f2040acf894a17c304e6794f940e01461a4c3
COZYkrish <cozykrish2916@gmail.com>
2026-03-27 19:44:20 +0530

    Resolved merge conflit

--- auth.py
+++ auth.py
@@ -8,14 +8,14 @@ app.secret_key = 'your_secret_key_here'
 
 USERS_FILE = 'users.json'
 
-def load_users():
-    if os.path.exists(USERS_FILE):
-        with open(USERS_FILE, 'r') as f:
-            try:
-                return json.load(f)
-            except json.JSONDecodeError:
-                return {}
-    return {}
+# def load_users():
+#     if os.path.exists(USERS_FILE):
+#         with open(USERS_FILE, 'r') as f:
+#             try:
+#                 return json.load(f)
+#             except json.JSONDecodeError:
+#                 return {}
+#     return {}
 
 def save_users(users):
     with open(USERS_FILE, 'w') as f:
