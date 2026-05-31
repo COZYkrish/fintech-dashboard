b3ef7e433f70a14242d111fd8fc1c6710a579193
COZYkrish <cozykrish2916@gmail.com>
2026-05-31 10:00:00 +0530

    feat: project updates

--- auth.py
+++ auth.py
@@ -9,13 +9,13 @@ app.secret_key = 'your_secret_key_here'
 USERS_FILE = 'users.json'
 
 def load_users():
-    if os.path.exists(USERS_FILE):
-        with open(USERS_FILE, 'r') as f:
-            try:
-                return json.load(f)
-            except json.JSONDecodeError:
-                return {}
-    return {}
+    # if os.path.exists(USERS_FILE):
+    #     with open(USERS_FILE, 'r') as f:
+    #         try:
+    #             return json.load(f)
+    #         except json.JSONDecodeError:
+    #             return {}
+    # return {}
 
 def save_users(users):
     with open(USERS_FILE, 'w') as f:
